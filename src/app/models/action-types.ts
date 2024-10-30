529516e145818b8910c127293e48855037ab6c41
Raul Alonso <c754s@ferrovial.com>
2024-10-30 08:43:00 +0100

    ecoputos instead of puntos

--- src/app/models/action-types.ts
+++ src/app/models/action-types.ts
@@ -1,11 +1,11 @@
 import { ActionType } from './action.model';
 
 export const actionTypes: ActionType[] = [
-  { id: 'bici', name: 'Ir a ensayar en bici (3 puntos)', points: 3 },
-  { id: 'andando', name: 'Ir a ensayar andando (3 puntos)', points: 3 },
+  { id: 'bici', name: 'Ir a ensayar en bici (3 ecopuntos)', points: 3 },
+  { id: 'andando', name: 'Ir a ensayar andando (3 ecopuntos)', points: 3 },
   {
     id: 'blablacar',
-    name: 'Ir a ensayar en coche compartido (1 punto)',
+    name: 'Ir a ensayar en coche compartido (1 ecopunto)',
     points: 1,
   },
 ];
