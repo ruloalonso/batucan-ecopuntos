eb8a23c1d7fbb248e9e9fd27e9fbcfe9e1891d2d
Raul Alonso <c754s@ferrovial.com>
2024-11-16 15:01:55 +0100

    meta tags

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
-import { ActionFormComponent } from "../action-form/action-form.component";
-import { UserListComponent } from "../user-list/user-list.component";
-import { ActionListComponent } from "../action-list/action-list.component";
+import { Component, OnInit } from '@angular/core';
+import { ActionFormComponent } from '../action-form/action-form.component';
+import { UserListComponent } from '../user-list/user-list.component';
+import { ActionListComponent } from '../action-list/action-list.component';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [ActionFormComponent, UserListComponent, ActionListComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private meta: Meta, private title: Title) {}
 
+  ngOnInit(): void {
+    this.title.setTitle('Título de tu web app');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: 'Tu app de confianza hecha con 💚 por Batucán',
+      },
+      { property: 'og:title', content: 'BATUCÁN Ecopuntos' },
+      {
+        property: 'og:description',
+        content: 'Welcome to la fiesta de los ecopuntos!',
+      },
+      { property: 'og:image', content: 'https://i.ibb.co/dkJYNGz/header.png' },
+      { property: 'og:url', content: 'https://batucan-ecopuntos.vercel.app' },
+      { property: 'og:type', content: 'website' },
+    ]);
+  }
 }
